b98927fc054119f323106e28de88ab43f9478e27
DanielArmR <daniel62armas@gmail.com>
2021-06-16 21:02:57 -0500

    Fist data types

---
+++ data-type/string.py
@@ -0,0 +1,8 @@
+message = 'Welcome to python, I´m a string'
+user_name = 'daniel692a'
+greeting = f'Hello {user_name}, welcome to python'
+print(greeting)
+
+full_name = 'Daniel Armas'
+msg = full_name + ' ' +  user_name
+print(msg)
